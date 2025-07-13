b8aab7cd988d11503921771914e8c89db57efc92
Pete Bunke <petebunke@gmail.com>
2025-07-12 19:26:07 -0700

    FIx width 6

--- pages/index.js
+++ pages/index.js
@@ -1732,8 +1732,8 @@ export default function RunwayAutomationApp() {
           {activeTab === 'setup' && (
             <div className="row justify-content-center">
               <div className="col-lg-10" style={{ maxWidth: '1200px', paddingLeft: '0', paddingRight: '0' }}>
-                <div className="row g-4">
-                  <div className="col-lg-6">
+                <div className="row g-4" style={{ marginLeft: '-12px', marginRight: '-12px' }}>
+                  <div className="col-lg-6" style={{ paddingLeft: '12px', paddingRight: '12px' }}>
                     <div className="card shadow-lg border-0" style={{ borderRadius: '8px', overflow: 'hidden' }}>
                       <div 
                         className="bg-primary position-relative d-flex align-items-center justify-content-center" 
@@ -1934,7 +1934,7 @@ export default function RunwayAutomationApp() {
                     </div>
                   </div>
 
-                  <div className="col-lg-6">
+                  <div className="col-lg-6" style={{ paddingLeft: '12px', paddingRight: '12px' }}>
                     <div className="card shadow-lg border-0" style={{ borderRadius: '8px', overflow: 'hidden' }}>
                       <div 
                         className="bg-primary position-relative d-flex align-items-center justify-content-center" 
