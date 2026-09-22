067bf70167bb46c6abde1b5f6cdb00d6fbf7c605
Pete Bunke <petebunke@gmail.com>
2026-09-21 20:44:16 -0700

    Refactor Google Analytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -34,18 +34,25 @@ export default function App({ Component, pageProps }) {
         <meta name="msapplication-navbutton-color" content="#667eea" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
- <script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZV0SP66YN" />
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-8ZV0SP66YN');
-    `,
-  }}
-/>
+import Script from 'next/script';
+export default function Home() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-8ZV0SP66YN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8ZV0SP66YN');
+        `}
+      </Script>
+    </>
+  );
+}
       <Component {...pageProps} />
     </>
   )
