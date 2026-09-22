3a4c84c9842f1244b6decff204090c9c0e8def3b
Pete Bunke <petebunke@gmail.com>
2026-09-21 20:26:40 -0700

    Add Google Tag Manager script to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,18 @@ export default function App({ Component, pageProps }) {
         <meta name="msapplication-navbutton-color" content="#667eea" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZV0SP66YN" />
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-8ZV0SP66YN');
+    `,
+  }}
+/>
      
       <Component {...pageProps} />
     </>
